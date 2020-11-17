17c07eada951e64064406c93f9eba55e54b637f8
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-17 17:17:46 +0100

    Dice Game

--- assets/js/script.js
+++ assets/js/script.js
@@ -43,6 +43,36 @@ btnSearch.addEventListener('click',
 
 // DICE GAME
 
+var playerDice = document.getElementById('player_dice');
+var iaDice = document.getElementById('ia_dice');
+var whoWin = document.getElementById('whowin');
+
+var generateNumberPlayer = Math.floor(Math.random() * 6) + 1;
+var generateNumberIa = Math.floor(Math.random() * 6) + 1;
+
+playerDice.innerHTML = generateNumberPlayer;
+iaDice.innerHTML = generateNumberIa;
+
+
+
+
+
+
+if (generateNumberPlayer > generateNumberIa) {
+
+    
+    whoWin.innerHTML = ('Player won');
+
+}
+
+else if (generateNumberPlayer == generateNumberIa) {
+    
+    whoWin.innerHTML = ('Draw match');
+}
+
+else {
+    whoWin.innerHTML = ('IA won');
+}
 
 
 // /DICE GAME
