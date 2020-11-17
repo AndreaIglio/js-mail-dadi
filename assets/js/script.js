ec3033660deded31d4fae2ab361890c19bf18e84
Andrea Iglio <andrea.iglio@gmail.com>
2020-11-17 16:56:03 +0100

    Email Game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,48 @@
+// EMAIL GAME
+
+
 // Variabili con valore
 
+var btnSearch = document.getElementById('btnsearch');
 
-// Variabili senza valore
+var userEmail = document.getElementById('useremail');
+console.log(userEmail);
+
+
+// Variabili con id associato all input
+var userEmailInput = document.getElementById('userEmailInput');
+console.log(userEmailInput);
+
+
+// Array
+
+var emailList = ["boolean@gmail.com", "birillo@gmail.com","storto@gmail.com"]
+
+// Event Listener
+
+btnSearch.addEventListener('click', 
+
+    function () {
+
+        for (var i = 0 ; i < emailList.length; i++) {
+
+            if (userEmail.value == emailList[i]) {
+
+            userEmailInput.innerHTML = ('You are good to go');
+            console.log(userEmailInput);
+
+            }
+        }
+    }
+)
+
+
+// /EMAIL GAME
+
+
+
+// DICE GAME
+
+
+
+// /DICE GAME
